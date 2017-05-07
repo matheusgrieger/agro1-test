46091c21647eec6807b8034f0af81efbcfa629c5
matheusgrieger <matheusgrieger@hotmail.com>
2017-05-07 18:02:48 -0300

    little fixes to compilation setup
    and added comments

--- index.js
+++ index.js
@@ -5,22 +5,32 @@ const path = require('path');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 
+// creates spinner to show progress
 let spinner = false;
 
 if (!argv.dev) {
     spinner = ora('building for production...');
+    // starts spinner
     spinner.start();
 }
 
+
+// clean build folder
+// keep only what matters
 rm(path.join('./build/**/*'), err => {
     if (err) throw err;
+
+    // starts webpack
     webpack(webpackConfig, (err, status) => {
-        if (spinner) {
+        // stops spinner
+        if (!argv.dev) {
             spinner.stop();
         }
 
+        // shows error if any
         if (err) throw err;
 
+        // logs results
         process.stdout.write(status.toString({
             colors: true,
             modules: false,
@@ -29,6 +39,7 @@ rm(path.join('./build/**/*'), err => {
             chunkModules: false
         }) + '\n\n');
 
+        // if on dev mode, remembers user that webpack is still watching file changes
         if (argv.dev) {
             console.log('webpack watching for file changes');
         }
--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,121 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
-module.exports = {
+// plugins used by production build
+function prodPlugins() {
+    return [
+        // compresses JS
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        // extracts all CSS into separate file
+        new ExtractTextPlugin({
+            filename: 'css/[name].[contenthash].css',
+            allChunks: true
+        }),
+        // optimizes CSS to reduce size
+        new OptimizeCSSPlugin({
+            cssProcessorOptions: {
+                safe: true // disables risky aggregations
+            }
+        }),
+        // injects generated files into template HTML file
+        // can also be configured to reduce HTML file size
+        new HtmlWebpackPlugin({
+            filename: path.resolve('./build/index.html'),
+            template: 'index.html',
+            inject: true,
+            chunksSortMode: 'dependency'
+        }),
+        // divides common code into separate files
+        // optimizes caching
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: (module, count) => {
+                return (
+                    module.resource &&
+                    /\.js$/.test(module.resource) &&
+                    module.resource.indexOf(
+                        path.resolve('./node_modules')
+                    ) === 0
+                )
+            }
+        }),
+        // creates separated file for webpack's bootstrap
+        // avoid using it in every file
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest',
+            chunk: ['vendor']
+        })
+    ];
+}
+
+// plugins used by development build
+function devPlugins() {
+    return [
+        // extracts all CSS into separate file
+        new ExtractTextPlugin({
+            filename: 'css/[name].css',
+            allChunks: true
+        }),
+        // injects generated files into template HTML file
+        new HtmlWebpackPlugin({
+            filename: path.resolve('./build/index.html'),
+            template: 'index.html',
+            inject: true,
+            chunksSortMode: 'dependency'
+        }),
+        // divides common code into separate files
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: (module, count) => {
+                return (
+                    module.resource &&
+                    /\.js$/.test(module.resource) &&
+                    module.resource.indexOf(
+                        path.resolve('./node_modules')
+                    ) === 0
+                )
+            }
+        }),
+        // creates separated file for webpack's bootstrap
+        // avoid using it in every file
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest',
+            chunk: ['vendor']
+        })
+    ];
+}
+
+// base webpack configuration
+var webpackBaseConfig = {
+    // main file
     entry: {
         main: path.resolve('./js/main.js')
     },
+    // destination path
+    // file names merged afterwards
     output: {
-        path: path.resolve('./build'),
-        filename: 'js/[name].[chunkhash].js',
-        chunkFilename: 'js/[id].[chunkhash].js'
+        path: path.resolve('./build')
     },
+    // shortcut to project root
     resolve: {
         extensions: ['.js', '.json'],
         alias: {
-            'root': path.resolve('./')
+            '~': path.resolve('./')
         }
     },
     module: {
         rules: [
+            // pure-CSS loader
+            // without preprocessors
             {
                 test: /\.css$/,
+                // being extracted
                 use: ExtractTextPlugin.extract({
                     use: [
                         {
@@ -36,13 +131,15 @@ module.exports = {
                             }
                         },
                         {
-                            loader: 'postcss-loader'
+                            loader: 'postcss-loader' // adds required vendor prefixes
                         }
                     ]
                 })
             },
+            // SCSS loader
             {
                 test: /\.scss$/,
+                // also being extracted
                 use: ExtractTextPlugin.extract({
                     use: [
                         {
@@ -60,6 +157,7 @@ module.exports = {
                         {
                             loader: 'sass-loader',
                             options: {
+                                // paths where to look for @import's
                                 includePaths: ['scss', 'node_modules']
                             }
                         }
@@ -67,47 +165,40 @@ module.exports = {
                 })
             }
         ]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new ExtractTextPlugin({
-            filename: 'css/[name].[contenthash].css',
-            allChunks: true
-        }),
-        new OptimizeCSSPlugin({
-            cssProcessorOptions: {
-                safe: true
-            }
-        }),
-        new HtmlWebpackPlugin({
-            filename: path.resolve('./build/index.html'),
-            template: 'index.html',
-            inject: true,
-            chunksSortMode: 'dependency'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: (module, count) => {
-                return (
-                    module.resource &&
-                    /\.js$/.test(module.resource) &&
-                    module.resource.indexOf(
-                        path.resolve('./node_modules')
-                    ) === 0
-                )
-            }
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'manifest',
-            chunk: ['vendor']
-        })
-    ],
-    watch: argv.dev,
-    watchOptions: {
-        aggregateTimeout: 300
     }
+};
+
+// production build
+if (!argv.dev) {
+    // merges base config with following specifics
+    module.exports = merge(webpackBaseConfig, {
+        // set to subfolder "js" using chunkhash to allow caching
+        output: {
+            filename: 'js/[name].[chunkhash].js',
+            chunkFilename: 'js/[id].[chunkhash].js'
+        },
+        // get all production plugins
+        plugins: prodPlugins()
+    });
+}
+
+// dev build
+else {
+    // merges base config with following specifics
+    module.exports = merge(webpackBaseConfig, {
+        // set to subfolder "js" without chunkhash
+        // this will make webpack overwrite the files on every build
+        // preventing lots of files from being created
+        output: {
+            filename: 'js/[name].js',
+            chunkFilename: 'js/[id].js'
+        },
+        // get all development plugins
+        plugins: devPlugins(),
+        // sets up watch with 300ms interval between saving and recompiling
+        watch: true,
+        watchOptions: {
+            aggregateTimeout: 300
+        }
+    });
 }
